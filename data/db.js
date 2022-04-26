caee77bd5ce1d5194ec6109ac8d49f617506ac20
gharbiamine <gharbiamine5674@gmail.com>
2022-04-27 00:57:03 +0100

    exo56

--- data/db.js
+++ data/db.js
@@ -1,44 +1,72 @@
 export const db = {
-    students : [
-        {
-            id: 1,
-            name: 'Fatma Laribi',
-            age: 22,
-            gender: "FEMALE",
-            classroom: 1
-        },
-        {
-            id: 2,
-            name: 'Ahmed Mahfoudhi',
-            age: 22,
-            gender: "MALE",
-            classroom: 2
-        },
-        {
-            id: 3,
-            name: 'Nadine Boukadida',
-            age: 22,
-            gender: "FEMALE",
-            classroom: 3
-        },
-        {
-            id: 4,
-            name: 'Mohamed Aziz Khayati',
-            age: 22,
-            gender: "MALE",
-            classroom: 1
-        }
-    ],
-    classroom: [
-        {
-            id: 1,
-            designation: 'GL3'
-        },{
-            id: 2,
-            designation: 'GL2'
-        },{
-            id: 3,
-            designation: 'GL4'
-        },
-    ]
-}
+  students: [
+    {
+      id: 1,
+      name: "Fatma Laribi",
+      age: 22,
+      gender: "FEMALE",
+      classroom: 1,
+    },
+    {
+      id: 2,
+      name: "Ahmed Mahfoudhi",
+      age: 22,
+      gender: "MALE",
+      classroom: 2,
+    },
+    {
+      id: 3,
+      name: "Nadine Boukadida",
+      age: 22,
+      gender: "FEMALE",
+      classroom: 3,
+    },
+    {
+      id: 4,
+      name: "Mohamed Aziz Khayati",
+      age: 22,
+      gender: "MALE",
+      classroom: 1,
+    },
+  ],
+  classroom: [
+    {
+      id: 1,
+      designation: "GL3",
+    },
+    {
+      id: 2,
+      designation: "GL2",
+    },
+    {
+      id: 3,
+      designation: "GL4",
+    },
+  ],
+  todos: [
+    {
+      id: "1",
+      name: "name1",
+      content: "content1",
+      status: "DONE",
+    },
+    {
+      id: "2",
+      name: "name2",
+      content: "content2",
+      status: "WAITING",
+    },
+    {
+      id: "3",
+      name: "name3",
+      content: "content3",
+      status: "IN_PROGRESS",
+    },
+    {
+      id: "4",
+      name: "name4",
+      content: "content4",
+      status: "CANCELED",
+    },
+  ],
+};
--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,25 +1,26 @@
 import { db } from "../data/db.js";
 
 export const Query = {
-    hello: (parent, args, context, info) => {
-        console.log('parent : ', parent);
-        console.log('context : ', context);
-        console.log('info : ', info);
-        console.log('args : ', args);
-        return `Hello ${args.name || 'World'}`;
-    },
-    infos: () => {
-        return {
-            name: 'Sellaouti',
-            firstname: 'Aymen'
-        }
-    },
-    getAllStudents: () => {
-        return db.students;
-    },
-    getStudent: (_, {id}) => {
-        return db.students.find(
-            (student) => student.id == id
-        );
-    }
-}
+  hello: (parent, args, context, info) => {
+    console.log("parent : ", parent);
+    console.log("context : ", context);
+    console.log("info : ", info);
+    console.log("args : ", args);
+    return `Hello ${args.name || "World"}`;
+  },
+  infos: () => {
+    return {
+      name: "Sellaouti",
+      firstname: "Aymen",
+    };
+  },
+  getAllStudents: () => {
+    return db.students;
+  },
+  getStudent: (_, { id }) => {
+    return db.students.find((student) => student.id == id);
+  },
+  getToDo: (_, { id }) => {
+    return db.todos.find((todo) => todo.id == id);
+  },
+};
